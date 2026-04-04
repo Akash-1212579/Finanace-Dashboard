97404aded98870ba94b6a26c1854d38e3efb6db7
Akash <dalbhanjanakash1@gmail.com>
2026-04-04 15:59:11 +0530

    implemented redis for cache

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { connectRedis } = require("../config/redis.config");
 const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
+connectRedis();
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}`);
 })
